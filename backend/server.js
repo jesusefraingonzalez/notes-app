d6d0ec5ae06935a86e9c2f7ec417a08dc012da3c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-10 03:14:24 -0500

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // require expressjs
 const express = require('express');
+const fs = require ('fs');
 
 // set up express
 const app = express();
@@ -7,14 +8,38 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (request, response) => {
-    console.log(request, response);
+// //open db.json
+// fs.open('db.json', (err, fd) => {
+//     if (err) throw err;
+//     console.log(fd);
+// });
+
+// returns the notes.html file
+app.get('/notes', (req, res) => {
+    return ('../public/notes.html');
 });
 
-app.get('*', (request, response) => {
-    console.log(request, response);
+// returns the index.html file
+app.get('*', (req, res) => {
+    return ('../public/index.html');
 });
 
+// route for getting notes
+app.get('/api/notes' , (req, res) => {
+
+});
+
+// route for posting new notes
+app.post('/api/notes', (req, res) => {
+   
+});
+
+// route for deleting notes
+app.delete('/api/notes:id', (req, res) => {
+
+});
+
+//starts server
 app.listen(PORT , () => {
     console.log(`Server now listening on port ${PORT}`);
 });
