9267ca2b5ee22c7fa1ed5b0de53eb0e7d70c0f3b
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-09 23:31:37 -0500

    add app.listen and app.get fns

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,19 @@
 const express = require('express');
 
 // set up express
-let app = express();
-let port = 3000;
-app.use(express.urlencoded({extended: true}));
+const app = express();
+const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/notes', (request, response) => {
+    console.log(request, response);
+});
 
+app.get('*', (request, response) => {
+    console.log(request, response);
+});
 
+app.listen(PORT , () => {
+    console.log(`Server now listening on port ${PORT}`);
+});
